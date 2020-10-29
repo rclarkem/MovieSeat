447b3bf024f5aaee358bf409b8ce9eb4a8efd46d
Reina <r.clarkemitchell@gmail.com>
2020-10-29 17:36:00 -0400

    js  skeleton

---
+++ index.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+
+const count = document.querySelector('#count');
+const total = document.querySelector('#total');
+const movieSelect = document.querySelector('#movie');
+
+const ticketPrice = Number(movieSelect.value);
+
+container.addEventListener('click', (e) => {
+  console.log(e.target);
+});
