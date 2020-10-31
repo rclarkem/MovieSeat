ea78e3cb0875a00c5d05162f2dd1492d9abe9467
Reina <r.clarkemitchell@gmail.com>
2020-10-30 21:12:24 -0400

    count and total for seats and price.

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 
-const count = document.querySelector('#count');
+let count = document.querySelector('#count');
 const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
 
 const ticketPrice = Number(movieSelect.value);
 
 container.addEventListener('click', (e) => {
-  console.log(e.target);
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+    countSeats(e);
+  }
+  // ticketPriceCount(count);
 });
+
+function countSeats(e) {
+  // let seatsSelect = document.querySelectorAll('.row .seat.selected');
+  // console.log(seatsSelect);
+
+  // const seatsSelectLength = seatsSelect.length;
+
+  // count.innerText = seatsSelectLength;
+
+  // My way but can do it the way above to as an node list
+  if (e.target.classList.contains('selected') && !e.target.classList.contains('occupied')) {
+    count.innerText++;
+  } else if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    count.innerText--;
+  }
+  return (total.innerText = Number(count.innerText) * ticketPrice);
+}
+
+// function ticketPriceCount(seats) {
+//   console.log(Number(seats.innerText) * ticketPrice);
+// }
